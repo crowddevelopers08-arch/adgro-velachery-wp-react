cc32314520e6fa85461372524e8c52a8dfc3f1d5
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-03-24 11:12:06 +0530

    new hair vwlached

--- components/thanknavbar.tsx
+++ components/thanknavbar.tsx
@@ -15,7 +15,7 @@ declare global {
 type Props = {
   logoSrc?: string;
   phone?: string;   // "+91 7409256789"
-  buttonText?: string; // "Call Now"
+  buttonText?: string; 
 };
 
 export default function ThankTopBar({
