4add3dff99ac12a2ffb19e64d4f600bb6fcacf94
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-09-03 14:23:47 +0530

    adgro hair velachry gtags

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,6 +63,26 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${outfit.variable} antialiased`}>
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W3BNH276');
+            `,
+          }}
+        />
+
         {/* Google Analytics 4 - G-FR90F9TQWP */}
         <Script
           strategy="afterInteractive"
@@ -112,6 +132,16 @@ export default function RootLayout({
         </Script>
       </head>
       <body className="font-sans">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W3BNH276"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         {children}
       </body>
     </html>
